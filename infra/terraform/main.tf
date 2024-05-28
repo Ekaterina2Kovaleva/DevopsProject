a83a8ebc4473884e7965f475e35e0a49d9447c3b
Katya <nz030432@gmail.com>
2024-05-29 02:49:50 +0500

    edit terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,49 +4,20 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  required_version = ">= 0.13"
 }
- 
+
 provider "yandex" {
   token  =  "y0_AgAAAAA_o5jkAATuwQAAAAEEx9JZAAB6OrExVZBKh5do3DZDsqMIGhPXfw"
   cloud_id  = "b1gn5dqg9b1gl8qjlmug"
   folder_id = "b1grbnvt6pt9nqtrir0o"
   zone      = "ru-central1-a"
 }
- 
-variable "image-id" {
-  type = string
-}
- 
-resource "yandex_compute_instance" "vm-1" {
-  name = "design-site-vm"
-  platform_id = "standard-v1"
-  zone = "ru-central1-a"
- 
-  resources {
-    cores  = 2
-    memory = 2
-  }
- 
-  boot_disk {
-    initialize_params {
-      image_id = var.image-id
-    }
-  }
- 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
- 
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
- 
+
 resource "yandex_vpc_network" "network-1" {
   name = "design-site-network"
 }
- 
+
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "design-site-subnet"
   zone           = "ru-central1-a"
@@ -70,7 +41,7 @@ resource "yandex_mdb_postgresql_cluster" "db-1" {
   name        = "design-site-db"
   environment = "PRESTABLE"
   network_id  = yandex_vpc_network.network-1.id
- 
+
   config {
     version = 12
     resources {
@@ -87,12 +58,12 @@ resource "yandex_mdb_postgresql_cluster" "db-1" {
       shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
     }
   }
- 
+
   database {
     name  = var.db-name
     owner = var.db-user
   }
- 
+
   user {
     name       = var.db-user
     password   = var.db-password
@@ -105,17 +76,84 @@ resource "yandex_mdb_postgresql_cluster" "db-1" {
       log_min_duration_statement    = 5000
     }
   }
- 
+
   host {
     zone      = "ru-central1-a"
     subnet_id = yandex_vpc_subnet.subnet-1.id
   }
 }
- 
+
+variable "image-id" {
+  type = string
+}
+
+variable "db-engine" {
+  type = string
+}
+
+variable "db-port" {
+  type = string
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "design-site-vm"
+  platform_id = "standard-v1"
+  zone = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image-id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  depends_on = [yandex_mdb_postgresql_cluster.db-1]
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/ubuntu/design_site"
+    ]
+  }
+
+  provisioner "file" {
+    source      = "../../infra"
+    destination = "/home/ubuntu/design_site"
+  }
+
+  provisioner "file" {
+    source      = "../../backend"
+    destination = "/home/ubuntu/design_site"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'DB_NAME=${var.db-name}' > /home/ubuntu/design_site/infra/.env",
+      "echo 'POSTGRES_USER=${var.db-user}' >> /home/ubuntu/design_site/infra/.env",
+      "echo 'POSTGRES_PASSWORD=${var.db-password}' >> /home/ubuntu/design_site/infra/.env",
+      "echo 'DB_HOST=${yandex_mdb_postgresql_cluster.db-1.host}' >> /home/ubuntu/design_site/infra/.env",
+      "echo 'DB_ENGINE=${var.db-engine}' >> /home/ubuntu/design_site/infra/.env",
+      "echo 'DB_PORT=${var.db-port}' >> /home/ubuntu/design_site/infra/.env"
+    ]
+  }
+}
+
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
- 
+
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
