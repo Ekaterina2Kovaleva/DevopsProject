d6f7e34ef05dd18d9a4d038989b8efe1877aa8c8
EkaterinaKovaleva <kovaleva.ekaterina03@mail.ru>
2024-09-09 19:30:24 +0500

    backend

--- backend/Design/urls.py
+++ backend/Design/urls.py
@@ -21,6 +21,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from project.views import ProjectViewSet,  PhotoViewSet, RequestViewSet, TariffViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -60,3 +62,5 @@ urlpatterns = [
         name='schema-redoc'
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
