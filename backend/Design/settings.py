c5dd1d860cad354658019192cc67f20a5254c0db
Dauglai <danil.samartsev.04@gmail.com>
2024-09-12 19:03:05 +0500

    add multiple photos

--- backend/Design/settings.py
+++ backend/Design/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v3ndu5pxbictx)as=n-9hmn4yog18bd5!+-a@4qqz!qipwgz63'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
