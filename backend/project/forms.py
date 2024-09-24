d3023a7d0596a4fb2cbf12069f54df89002b1680
Dauglai <danil.samartsev.04@gmail.com>
2024-09-24 13:14:09 +0500

    new form

--- backend/project/forms.py
+++ backend/project/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from django.core.validators import validate_image_file_extension
 from django.utils.translation import gettext as _
@@ -5,20 +6,39 @@ from django.utils.translation import gettext as _
 
 from .models import Project, Photo, DesignPhoto
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+class FileFieldForm(forms.Form):
+    file_field = MultipleFileField()
 
 class ProjectAdminForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
 
-    photos = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
-        label=_("Добавить фотографии"), 
+    photos = MultipleFileField(
+        widget=MultipleFileInput(),
+        label=_("Добавить фотографии"),
         required=False,
     )
 
-    design_photos = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
+    design_photos = MultipleFileField(
+        widget=MultipleFileInput(),
         label=_("Добавить дизайн-фотографии"), 
         required=False,
     )
